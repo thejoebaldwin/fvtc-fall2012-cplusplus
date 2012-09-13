f96b16d6087a256269fc8a281b802df9d462835e
Joe Baldwin <contact@humboldttechgroup.com>
2012-09-13 11:34:09 -0500

    Updated to larger font size

--- 09_10_12/ColorOutput/ColorOutput/main.cpp
+++ 09_10_12/ColorOutput/ColorOutput/main.cpp
@@ -12,14 +12,45 @@ int main()
 	//console reference	
 	HANDLE	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	//this will loop until either 0 is entered, or a non-numeric value
-	while (color != 0)
-	{
-		cout << "Enter a number to see what color you get:" << endl; 
-		//accept input for user
-		cin >> color;	
-		//set the color of the console output
-		SetConsoleTextAttribute(hConsole, color); 
-	}
+
+   PCONSOLE_FONT_INFOEX font = new CONSOLE_FONT_INFOEX();
+   font->cbSize = sizeof(CONSOLE_FONT_INFOEX);
+   
+   //CONSOLE_FONT_INFOEX is defined in some windows header
+    GetCurrentConsoleFontEx(hConsole, false, font);
+	//PCONSOLE_FONT_INFOEX is the same as CONSOLE_FONT_INFOEX*
+    font->dwFontSize.X = 10;
+    font->dwFontSize.Y = 18;
+    
+	
+
+        //SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), NULL, lpConsoleCurrentFontEx);
+
+
+	
+	
+	SetCurrentConsoleFontEx(hConsole, false, font);
+    //SetConsoleTextAttribute(hConsole, color); 
+    //SetConsoleTextAttribute(hConsole, 0x0C);
+
+
+
+
+	//while (color < 255)
+	//{
+	//	cout << "Using Color:" << color << endl; 
+	//	//accept input for user
+	//	//cin >> color;	
+	//	color++;
+	//	//set the color of the console output
+	//	SetConsoleTextAttribute(hConsole, color); 
+	//}
+	SetConsoleTextAttribute(hConsole, 240); 
+		
+		
+	cout << font->FaceName << endl;
+
+	cout << "hello";
 	_getch();
 	return 0;
 }
