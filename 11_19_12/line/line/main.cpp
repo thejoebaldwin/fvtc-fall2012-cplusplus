3827a3339df9af84418b2ad8987d33263d7acb18
thejoebaldwin <contact@humboldttechgroup.com>
2012-11-19 14:12:30 -0600

    array with null values

--- 11_19_12/line/line/main.cpp
+++ 11_19_12/line/line/main.cpp
@@ -7,16 +7,16 @@ using namespace std;
 
 int main()
 {
+
+
+	
+
+
 	int width;
 	int height;
 
 
-	char cubed = 254;
 
-	string hello = "power of " + cubed;
-
-	cout << cubed << endl;
-	_getch();
 		
 
 	cout << "Please enter a line height:";
@@ -27,22 +27,10 @@ int main()
 	
 	line l(width, height);
 
-	//l.setHeight(height);
-	//l.setWidth(width);
+	
 	cout << l.toString();
 
 	
-	/*line l;
-	l.setWidth(10);
-	l.setHeight(1);
-	cout << l.toString() << endl;
-
-	line myLine;
-	myLine.setHeight(20);
-	myLine.setWidth(4);
-	cout << myLine.toString() << endl;*/
-
-
 
 	_getch();
 	return 0;
